36c53a434f77cec0ce5a4891351e59a4f7e909bd
Hjarambulo <hj.arambulo92@gmail.com>
2022-05-22 22:18:18 -0300

    credentials moved

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,15 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { GoogleAuthProvider } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDIDdg1JRCEN8aI6ng9qMvluqSn_nUaHpk',
-  authDomain: 'react-journal-app-a8d9d.firebaseapp.com',
-  projectId: 'react-journal-app-a8d9d',
-  storageBucket: 'react-journal-app-a8d9d.appspot.com',
-  messagingSenderId: '41585561633',
-  appId: '1:41585561633:web:b9efbdc1422f42bcf85360',
-};
+import { firebaseConfig } from '../../firebaseConfig';
 
 const app = initializeApp(firebaseConfig);
 
